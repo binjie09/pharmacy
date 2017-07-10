85017ede6e3744ce1785aecea1ddc0e4bb034405
yidaqi <yidaqi@yahoo.com>
2017-07-11 04:10:41 +0800

    插入excel 方法更新

--- 药店报账工具/药店报账工具/ExcelHelper.cs
+++ 药店报账工具/药店报账工具/ExcelHelper.cs
@@ -20,41 +20,55 @@ namespace 药店报账工具
             /// </summary>
             /// <param name="dt"></param>
             /// <param name="file"></param>
-            public static void TableToExcelForXLS(DataTable dt, string file)
+            /// <param name="month"></param>
+            /// <param name="index"></param>
+            public static void TableToExcelForXLS(DataTable dt, string file, string month,int index)
             {
-                HSSFWorkbook hssfworkbook = new HSSFWorkbook();
-                ISheet sheet = hssfworkbook.CreateSheet("data");
-
-                //表头
-                IRow row = sheet.CreateRow(0);
-                for (int i = 0; i < dt.Columns.Count; i++)
+                try
                 {
-                    ICell cell = row.CreateCell(i);
-                    cell.SetCellValue(dt.Columns[i].ColumnName);
-                }
+                    HSSFWorkbook hssfworkbook = new HSSFWorkbook();
+                    ISheet sheet = hssfworkbook.CreateSheet(month);
 
-                //数据
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    IRow row1 = sheet.CreateRow(i + 1);
-                    for (int j = 0; j < dt.Columns.Count; j++)
+                    // 当且仅当第一次交易时创建表头
+                    if (index == 1)
                     {
-                        ICell cell = row1.CreateCell(j);
-                        cell.SetCellValue(dt.Rows[i][j].ToString());
+                        //表头
+                        IRow row = sheet.CreateRow(0);
+                        for (int i = 0; i < dt.Columns.Count; i++)
+                        {
+                            ICell cell = row.CreateCell(i);
+                            cell.SetCellValue(dt.Columns[i].ColumnName);
+                        }
+                    }
+
+                    //数据
+                    for (int i = index; i < dt.Rows.Count; i++)
+                    {
+                        IRow row1 = sheet.CreateRow(i + 1);
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            ICell cell = row1.CreateCell(j);
+                            cell.SetCellValue(dt.Rows[i][j].ToString());
+                        }
+                    }
+
+                    //转为字节数组
+                    MemoryStream stream = new MemoryStream();
+                    hssfworkbook.Write(stream);
+                    var buf = stream.ToArray();
+
+                    //保存为Excel文件
+                    using (FileStream fs = new FileStream(file, FileMode.Create, FileAccess.Write))
+                    {
+                        fs.Write(buf, 0, buf.Length);
+                        fs.Flush();
                     }
                 }
-
-                //转为字节数组
-                MemoryStream stream = new MemoryStream();
-                hssfworkbook.Write(stream);
-                var buf = stream.ToArray();
-
-                //保存为Excel文件
-                using (FileStream fs = new FileStream(file, FileMode.Create, FileAccess.Write))
+                catch(Exception e)
                 {
-                    fs.Write(buf, 0, buf.Length);
-                    fs.Flush();
+
                 }
+                
             }
 
 
