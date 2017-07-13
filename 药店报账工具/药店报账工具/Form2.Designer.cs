b5eb0cd8c0c816be440261e89b50eedcedcb310f
yidaqi <yidaqi@yahoo.com>
2017-07-13 11:53:39 +0800

    不是到修改了什么

--- 药店报账工具/药店报账工具/Form2.Designer.cs
+++ 药店报账工具/药店报账工具/Form2.Designer.cs
@@ -150,7 +150,7 @@
             this.button1.TabIndex = 11;
             this.button1.Text = "取消";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form2
             // 
